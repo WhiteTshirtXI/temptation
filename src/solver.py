9b1cd5552968f43791afed839e4f9db37cef8c27
Anand <natkhatanand@gmail.com>
2013-12-13 14:40:30 -0500

    working version, almost 200x faster with f2py

--- src/solver.py
+++ src/solver.py
@@ -1,4 +1,5 @@
 from pylab import *
+from utils import *
 class Solver(object):
     """
     
@@ -8,10 +9,8 @@ class Solver(object):
     """
     def __init__(self):
         # N is number of pressure points in the staggered grid
-        self.Ni = 40
-        self.Nj = 40
-        
-        self.Nb = 2
+        self.Ni = 80
+        self.Nj = 80
         
         self.Li = 1.0
         self.Lj = 1.0
@@ -24,133 +23,44 @@ class Solver(object):
         # 1: right face
         # 2: top face
         # 3: left face
-        self.u = zeros([self.Ni+1, self.Nj])
-        self.v = zeros([self.Ni, self.Nj-1])
+        self.u = zeros([self.Ni+1, self.Nj], order="F")
+        self.v = zeros([self.Ni, self.Nj-1], order="F")
         x = linspace(0, 1.0, self.Ni)
         y = linspace(0, 1.0, self.Ni)
         self.xx, self.yy = meshgrid(x, y)
         
-        self.p = zeros([self.Ni, self.Nj])
-        self.Re = 100.0
-        self.dt = 1e-3
+        self.p = zeros([self.Ni, self.Nj], order="F")
+        self.Re = 1000.0
+        self.dt = 1e-5
         self.beta = 0.8
-    def apply_boundary_conditions(self):
-        Ni = self.Ni
-        Nj = self.Nj
+        self.tol = 1e-4
+        self.ut = np.zeros_like(self.u)
+        self.vt = np.zeros_like(self.v)
+        self.uw = np.zeros(4, order="F")
+        self.uw[1] = 1.0
+        self.p_old = np.zeros_like(self.p)
+        self.residue = 1.0
 
-        for i in range(Ni+1):
-            for j in range(Nj):
-                if j in [0]:
-                    self.u[i,j] = -self.u[i,1]
-                elif j in [Nj-1]:
-                    self.u[i,j] = 1.0-self.u[i,Nj-2]
-                if i in [0]:
-                    self.u[i,j] = -self.u[1,j]
-                elif i in [Ni+1-1]:
-                    self.u[i,j] = -self.u[Ni+1-2,j]
-
-        for i in range(Ni):
-            for j in range(Nj-1):
-                if j in [0]:
-                    self.v[i,j] = -self.v[i,1]
-                elif j in [Nj-1-1]:
-                    self.v[i,j] = -self.v[i,Nj-1-2]
-                if i in [0]:
-                    self.v[i,j] = -self.v[1,j]
-                elif i in [Ni-1]:
-                    self.v[i,j] = -self.v[Ni-2,j] 
-                    
-    def calc_ut(self):
-        self.ut = copy(self.u)
-        for i in range(1, self.Ni+1-1):
-            for j in range(1, self.Nj-1):
-                a_1 = pow(0.5*(self.u[i+1,j] + self.u[i,j]), 2.0)
-                a_2 = pow(0.5*(self.u[i,j] + self.u[i-1,j]), 2.0)
-                a_3 = 0.25*(self.u[i,j] + self.u[i,j+1])*(self.v[i-1,j] + self.v[i,j])
-                a_4 = 0.25*(self.u[i,j] + self.u[i,j-1])*(self.v[i-1,j-1] + self.v[i,j-1])
-
-                d_1 = (self.u[i+1,j] - 2.0*self.u[i,j] + self.u[i-1,j])/self.dx/self.dx
-                d_2 = (self.u[i,j+1] - 2.0*self.u[i,j] + self.u[i,j-1])/self.dy/self.dy
-
-                A = (a_1 - a_2)/self.dx + (a_3 - a_4)/self.dy
-                D = 1/self.Re*(d_1 + d_2)
-                self.ut[i,j] = self.u[i,j] + self.dt*(-A+D)
-
-        
-    def calc_vt(self):
-        self.vt = copy(self.v)
-        for i in range(1, self.Ni-1):
-            for j in range(1, self.Nj-1-1):
-                a_1 = pow(0.5*(self.v[i,j+1] + self.v[i,j]),2.0)
-                a_2 = pow(0.5*(self.v[i,j] + self.v[i,j-1]),2.0)
-                a_3 = 0.25*(self.u[i+1,j] + self.u[i+1,j+1])*(self.v[i,j] + self.v[i+1,j])
-                a_4 = 0.25*(self.u[i,j] + self.u[i,j+1])*(self.v[i-1,j] + self.v[i,j])
-
-                d_1 = (self.v[i+1,j] - 2.0*self.v[i,j] + self.v[i-1,j])/self.dx/self.dx
-                d_2 = (self.v[i,j+1] - 2.0*self.v[i,j] + self.v[i,j-1])/self.dy/self.dy
-                
-                A = (a_1 - a_2)/self.dy + (a_3 - a_4)/self.dx
-                D = 1/self.Re*(d_1 + d_2)
-                self.vt[i,j] = self.v[i,j] + self.dt*(-A+D)
-
-    def calc_p(self):
-        Ni = self.Ni
-        Nj = self.Nj
-        q = zeros_like(self.p)
-        for i in range(1,Ni-1):
-            for j in range(1,Nj-1):
-                q[i,j] = ((self.ut[i+1,j] - self.ut[i,j])/self.dx + (self.vt[i,j] - self.vt[i,j-1])/self.dy)/self.dt
-        p = copy(self.p)
-        p_new = zeros_like(p)
-        while 1:
-            for i in range(1,Ni-1):
-                for j in range(1,Nj-1):
-                    p_new[i,j] = 0.25*self.beta*(p[i+1,j]+p[i-1,j]+p[i,j+1]+p[i,j-1] - self.dx*self.dx*q[i,j]) + (1-self.beta)*p[i,j]
-
-            if np.linalg.norm(p_new-p, np.inf) < 1e-3:
-                for j in range(Nj):
-                    p_new[0,j] = p_new[1,j]
-                    p_new[Ni-1,j] = p_new[Ni-2,j]
-                for i in range(Ni):
-                    p_new[i,0] = p_new[i,1]
-                    p_new[i,Nj-1] = p_new[i,Nj-2]
-
-       
-                p = copy(p_new)
-                break
-            else:
-                p = copy(p_new)
-        self.p = copy(p)
-
-    def update_uv(self):
-        for i in range(1, self.Ni+1-1):
-            for j in range(1, self.Nj-1):
-                self.u[i,j] = self.ut[i,j] - self.dt/self.dx*(self.p[i,j]-self.p[i-1,j])
-        for i in range(1, self.Ni-1):
-            for j in range(1, self.Nj-1-1):
-                self.v[i,j] = self.vt[i,j] - self.dt/self.dy*(self.p[i,j+1]-self.p[i,j])
-    
     def loop(self):
         u_old = copy(self.u)
         counter = 0
         while 1:
-            self.apply_boundary_conditions()
-            self.calc_ut()
-            self.calc_vt()
-            self.calc_p()
-            self.update_uv()
-            residue = abs(u_old-self.u).max()
-            if residue < 1e-6:
+            set_boundary(self.u, self.v, self.uw)
+            set_uv_t(self.u, self.v, self.ut, self.vt, self.Re, self.dx, self.dt)
+            poisson(self.p, self.ut, self.vt, self.dx, self.dt, self.beta, self.tol)
+            update_uv(self.u, self.v, self.ut, self.vt, self.p, self.dt, self.dx)
+            self.residue = abs(u_old-self.u).max()
+            if self.residue < 5e-6 and counter > 2 or counter > 10000:
                 break
             else:
-                print residue, counter
                 u_old = copy(self.u)
+                print self.residue, counter
                 counter +=1
-                if counter%10 == 0:
+                if counter%1000 == 0:
                     np.savetxt("u.txt", self.u)
                     np.savetxt("v.txt", self.v)
                     np.savetxt("p.txt", self.p)
-        contourf(self.u[2:-3,2:-3])
+        contourf(self.u[1:-2,1:-2])
         colorbar()
         show()
 
---
+++ src/utils.f95
@@ -0,0 +1,149 @@
+subroutine poisson(p, ut, vt, dx, dt, beta, tol, ni, nj)
+  implicit none
+  integer :: ni, nj, i, j
+  real(8), intent(in) :: beta, tol, dx, dt
+  real(8), intent(in), dimension(ni+1, nj) :: ut
+  real(8), intent(in), dimension(ni, nj-1) :: vt
+  real(8), intent(inout), dimension(ni, nj) :: p
+  real(8), dimension(ni,nj) :: p_new, p_old, q
+  p_old = p
+  p_new = p
+  do i=2, ni-1
+     do j=2, nj-1
+        q(i,j) = ((ut(i+1,j) - ut(i,j))/dx + (vt(i,j) - vt(i,j-1))/dx)/dt*dx*dx
+     enddo
+  enddo
+
+  do 
+     do i = 2, ni-1
+        do j = 2, nj-1
+           p_new(i,j) = 0.25*beta*(p(i+1,j) + p(i-1,j) + p(i,j+1) + p(i,j-1) - q(i,j)) + (1.0-beta)*p(i,j)
+        enddo
+     enddo
+     if (maxval(abs(p_new-p)) < tol) then
+        p = p_new
+        do i=1, ni
+           p(i,1) = p_new(i,2)
+           p(i,nj) = p_new(i,nj-1)
+        enddo
+        do j=1, nj
+           p(1,j) = p_new(2,j)
+           p(ni,j) = p_new(ni-1,j)
+        enddo
+        exit
+     else 
+        p = p_new
+     endif
+  enddo
+end subroutine poisson
+
+subroutine set_boundary(u, v, uw, ni, nj)
+  implicit none
+  integer ni, nj, i, j
+  real(8), intent(inout), dimension(ni+1, nj) :: u
+  real(8), intent(inout), dimension(ni, nj-1) :: v
+  real(8), intent(in), dimension(4) :: uw
+  
+  do i=1, ni+1
+     do j=1, nj
+        if (j.eq.1) then
+           u(i,j) = 2*uw(1)-u(i,2)
+        else if(j.eq.nj) then
+           u(i,j) = 2*uw(2)-u(i,nj-1)
+        endif
+
+        if (i.eq.1) then
+           u(i,j) = -u(2,j)
+        else if(i.eq.ni+1) then
+           u(i,j) = -u(ni,j)
+        endif
+     enddo
+  enddo
+
+  do i=1, ni
+     do j=1, nj
+        if (j.eq.1) then
+           v(i,j) = -v(i,2)
+        else if(j.eq.nj-1) then
+           v(i,j) = -v(i,nj-2)
+        endif
+
+        if (i.eq.1) then
+           v(i,j) = 2*uw(3)-v(2,j)
+        else if(i.eq.ni) then
+           v(i,j) = 2*uw(4)-v(ni-1,j)
+        endif
+     enddo
+  enddo
+     
+end subroutine set_boundary
+
+subroutine set_uv_t(u, v, ut, vt, ni, nj, Re, dx, dt)
+  implicit none
+  integer ni, nj, i, j
+  real(8) :: Re, dx, dy, dt
+  real(8), intent(in), dimension(ni+1, nj) :: u
+  real(8), intent(in), dimension(ni, nj-1) :: v
+  real(8), intent(inout), dimension(ni+1, nj) :: ut
+  real(8), intent(inout), dimension(ni, nj-1) :: vt
+  
+  real(8) :: a_1, a_2, a_3, a_4, d_1, d_2, A, D
+  dy = dx
+  do i=2, ni+1-1
+     do j=2, nj-1
+        a_1 = (0.5*(u(i+1,j) + u(i,j)))**2.0
+        a_2 = (0.5*(u(i,j) + u(i-1,j)))**2.0
+        a_3 = 0.25*(u(i,j) + u(i,j+1))*(v(i-1,j) + v(i,j))
+        a_4 = 0.25*(u(i,j) + u(i,j-1))*(v(i-1,j-1) + v(i,j-1))
+        
+        d_1 = (u(i+1,j) - 2.0*u(i,j) + u(i-1,j))/dx/dx
+        d_2 = (u(i,j+1) - 2.0*u(i,j) + u(i,j-1))/dy/dy
+        
+        A = (a_1 - a_2)/dx + (a_3 - a_4)/dy
+        D = 1/Re*(d_1 + d_2)
+        ut(i,j) = u(i,j) + dt*(-A+D)
+     enddo
+  enddo
+  
+  do i=2, ni-1
+     do j=2,nj-1-1
+        a_1 = (0.5*(v(i,j+1) + v(i,j)))**2.0
+        a_2 = (0.5*(v(i,j) + v(i,j-1)))**2.0
+        a_3 = 0.25*(u(i+1,j) + u(i+1,j+1))*(v(i,j) + v(i+1,j))
+        a_4 = 0.25*(u(i,j) + u(i,j+1))*(v(i-1,j) + v(i,j))
+        
+        d_1 = (v(i+1,j) - 2.0*v(i,j) + v(i-1,j))/dx/dx
+        d_2 = (v(i,j+1) - 2.0*v(i,j) + v(i,j-1))/dy/dy
+        
+        A = (a_1 - a_2)/dy + (a_3 - a_4)/dx
+        D = 1/Re*(d_1 + d_2)
+        vt(i,j) = v(i,j) +dt*(-A+D)
+     enddo
+  enddo
+end subroutine set_uv_t
+
+subroutine update_uv(u, v, ut, vt, p, dt, dx, ni, nj)
+  implicit none
+  integer ni, nj, i, j
+  real(8) :: dx, dy, dt
+  real(8), intent(inout), dimension(ni+1, nj) :: u
+  real(8), intent(inout), dimension(ni, nj-1) :: v
+  real(8), intent(in), dimension(ni+1, nj) :: ut
+  real(8), intent(in), dimension(ni, nj-1) :: vt
+  real(8), intent(in), dimension(ni, nj) :: p
+
+  dy = dx
+  do i=2, ni+1-1
+     do j=2, nj-1
+        u(i,j) = ut(i,j) - dt/dx*(p(i,j)-p(i-1,j))
+     enddo
+  enddo
+  
+  do i=2, ni-1
+     do j=2,nj-1-1
+        v(i,j) = vt(i,j) - dt/dy*(p(i,j+1)-p(i,j))
+     enddo
+  enddo
+  
+end subroutine update_uv
+    
